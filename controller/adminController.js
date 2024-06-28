2b0561dad0b8c237580a109e3f41c31f42916e6f
[Abhinavkunnummal] <[abhinavkkunnummal@gmail.com]>
2024-06-28 16:56:16 +0530

    product offer

--- controller/adminController.js
+++ controller/adminController.js
@@ -1263,8 +1263,9 @@ const editCategoryOffer = async (req, res) => {
 
 const updateCategoryOffer = async (req, res) => {
   try {
+    const offerId = req.params.id;
     const { offerName, discount, startDate, endDate, categoryId } = req.body;
-console.log("ethis ----"+req.body);
+console.log("ethis ----"+req.body.offerName);
     if (!offerName || !discount || !startDate || !endDate || !categoryId) {
       req.flash('error', 'Missing required fields');
       return res.redirect(`/admin/editCategoryOffer/${offerId}`);
