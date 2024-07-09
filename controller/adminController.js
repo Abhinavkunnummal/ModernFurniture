0b7f4216afb9d1633f9eb2156ee317f4542979d2
[Abhinavkunnummal] <[abhinavkkunnummal@gmail.com]>
2024-07-09 20:53:06 +0530

    error fix : sorted products

--- controller/adminController.js
+++ controller/adminController.js
@@ -341,29 +341,32 @@ const addProduct = async (req, res) => {
 
 const addingNewProduct = async (req, res) => {
   try {
-    const { name, description, price, stock, dateCreated, category } = req.body;
-    console.log(stock);
+    const { name, description, price, stock, category } = req.body;
     const categoryId = await Category.findOne({ _id: category, is_Listed: false });
     const categoryData = await Category.find({ is_Listed: false });
     if (!categoryId) {
-      return res.render("addProduct", { message: "Category not found or is not listed.", categoryData});
+      return res.render("addProduct", { message: "Category not found or is not listed.", categoryData });
     }
-    if(stock < 0){
-      return res.render("addProduct", { message: "Stock cannot be negative.", categoryData});
+    if (stock < 0) {
+      return res.render("addProduct", { message: "Stock cannot be negative.", categoryData });
     }
     const normalizedProductName = name.trim().toLowerCase();
     const existingProduct = await Product.findOne({ name: normalizedProductName });
     if (existingProduct) {
-      return res.render("addProduct", { message: "Product name already exists",categoryData});
+      return res.render("addProduct", { message: "Product name already exists", categoryData });
     }
+
     const croppedimages = [];
-    for (const file of req.files) {
-      const croppedBuffer = await sharp(file.path).resize({ width: 350, height: 450, fit: sharp.fit.cover }).toBuffer();
-      const filename = `cropped_${file.originalname}`;
-      croppedimages.push(filename);
-      await sharp(croppedBuffer).toFile(`public/productimage/${filename}`);
+    if (req.files && req.files.length > 0) {
+      for (const file of req.files) {
+        const filename = `cropped_${Date.now()}_${file.originalname}`;
+        const filepath = `public/productimage/${filename}`;
+        await fs.promises.writeFile(filepath, file.buffer);
+        croppedimages.push(filename);
+      }
     }
-    const product = new Product({ name, description, price, category: categoryId._id, stock, image: croppedimages, dateCreated });
+
+    const product = new Product({ name, description, price, category: categoryId._id, stock, image: croppedimages, dateCreated: new Date() });
     await product.save();
     res.redirect("/admin/productlist");
   } catch (error) {
@@ -371,6 +374,9 @@ const addingNewProduct = async (req, res) => {
     res.status(500).render('error');
   }
 };
+
+module.exports = addingNewProduct;
+
 
 //-------------------------------------------------------- DELETE IMAGE ---------------------------------------------------------//
 
